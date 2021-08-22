ade5ea08fbc584852ee1f3b65314e56f9b8a5259
Aram Yesildeniz <aram.yesildeniz@outlook.com>
2021-08-22 12:53:02 +0200

    k

--- ttfb.py
+++ ttfb.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from constants import *
@@ -5,12 +6,24 @@ from constants import *
 
 sns.set_theme()
 
+# Extract Data and merge everything in one DataFrame
+
 file_1 = ORIGINAL_PATH
 column_name = "TTFB"
 
 data_frame_1 = pd.read_csv(file_1)
 
-data_1_fv = data_frame_1[column_name].iloc[::2]
-data_1_rv = data_frame_1[column_name].iloc[1::2]
+data_1_fv = data_frame_1[column_name].iloc[::2].reset_index(drop=True)
+data_1_rv = data_frame_1[column_name].iloc[1::2].reset_index(drop=True)
 
-print(data_1_rv)
+data = pd.concat([data_1_fv, data_1_rv], keys=['TTFB_FV', 'TTFB_RV'], axis=1)
+print(data)
+
+
+# Plot
+
+sns.displot(data, kde=True)
+plt.show()
+
+
+# kind="ecdf" for cumulative distribution function

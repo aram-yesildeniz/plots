9a2ff2a3f8d94074e1c20b3bdac0254421b8ccf8
Aram Yesildeniz <aram.yesildeniz@outlook.com>
2021-08-22 16:41:18 +0200

    ok

--- constants.py
+++ constants.py
@@ -1,2 +1,26 @@
-ORIGINAL_PATH = "data/2021-05-28_Original/210528_BE_4_summary.csv"
-TEST_FILENAME = "210529_2T_1_summary.csv"
+ORIGINAL_WPT_PATH = "data/2021-05-28_Original/210528_BE_4_summary.csv"
+TEST_WPT_PATH = "data/2021-05-29_Test/210529_2T_1_summary.csv"
+
+P1_WPT_PATH = "data/2021-05-30_P1/210530_FB_1_summary.csv"
+P1_GA_PATH = "data/2021-05-30_P1/2021-05-30.csv"
+
+P2_WPT_PATH = "data/2021-05-31_P2/210531_CW_1_summary.csv"
+P2_GA_PATH = "data/2021-05-31_P2/2021-05-31.csv"
+
+P3_WPT_PATH = "data/2021-06-02_P3/210602_D0_1_summary.csv"
+P3_GA_PATH = "data/2021-06-02_P3/2021-06-02.csv"
+
+A1_WPT_PATH = P1_WPT_PATH
+A1_GA_PATH = P1_GA_PATH
+
+A2_WPT_PATH = "data/2021-06-04_A2/210604_51_1_summary.csv"
+A2_GA_PATH = "data/2021-06-04_A2/2021-06-04.csv"
+
+A3_WPT_PATH = "data/2021-06-09_A3/210609_R2_1_summary.csv"
+A3_GA_PATH = "data/2021-06-09_A3/2021-06-09.csv"
+
+OS1_WPT_PATH = P1_WPT_PATH
+OS1_GA_PATH = P1_GA_PATH
+
+OS2_WPT_PATH = "data/2021-06-03_OS2/210603_P0_1_summary.csv"
+OS2_GA_PATH = "data/2021-06-03_OS2/2021-06-03.csv"
--- ttfb.py
+++ ttfb.py
@@ -8,22 +8,54 @@ sns.set_theme()
 
 # Extract Data and merge everything in one DataFrame
 
-file_1 = ORIGINAL_PATH
+file_1 = OS1_WPT_PATH
+file_2 = OS2_WPT_PATH
+# file_3 = A3_WPT_PATH
+
 column_name = "TTFB"
 
 data_frame_1 = pd.read_csv(file_1)
+data_frame_2 = pd.read_csv(file_2)
+# data_frame_3 = pd.read_csv(file_3)
 
 data_1_fv = data_frame_1[column_name].iloc[::2].reset_index(drop=True)
 data_1_rv = data_frame_1[column_name].iloc[1::2].reset_index(drop=True)
 
-data = pd.concat([data_1_fv, data_1_rv], keys=['TTFB_FV', 'TTFB_RV'], axis=1)
+data_2_fv = data_frame_2[column_name].iloc[::2].reset_index(drop=True)
+data_2_rv = data_frame_2[column_name].iloc[1::2].reset_index(drop=True)
+
+# data_3_fv = data_frame_3[column_name].iloc[::2].reset_index(drop=True)
+# data_3_rv = data_frame_3[column_name].iloc[1::2].reset_index(drop=True)
+
+
+# data = pd.concat([data_1_fv, data_2_fv, data_3_fv], keys=[
+#                  'P1 TTFB FV', 'P2 TTFB FV', 'P3 TTFB FV'], axis=1)
+
+# data = pd.concat([data_1_rv, data_2_rv, data_3_rv], keys=[
+#                  'P1', 'P2', 'P3'], axis=1)
+
+data = pd.concat([data_1_rv, data_2_rv], keys=[
+                 'OS1', 'OS2'], axis=1)
+
 print(data)
 
 
 # Plot
 
-sns.displot(data, kde=True)
-plt.show()
+g = sns.displot(
+    data,
+    kind="kde",
+    fill=True)
+
+g.set_axis_labels("Time (ms)")
+g.set(title='TTFB Repeat View')
+
+# sns.displot(data, kind="ecdf")
+
+
+# plt.show()
+plt.tight_layout()
+plt.gcf().savefig("__output.pdf", format='pdf')
 
 
 # kind="ecdf" for cumulative distribution function
